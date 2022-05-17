c98e86cb916640df38274e7b551a377b1c5f9e58
GaetanPe <gaetan.peronnet@gmail.com>
2022-05-17 17:21:49 +0200

    hero display

--- main.lua
+++ main.lua
@@ -3,20 +3,22 @@ io.stdout: setvbuf('no')
 
 love.graphics.setDefaultFilter("nearest")
 
-if arg[#arg] == "-debug" then require("modebug").start() end
+if arg[#arg] == "-debug" then require("mobdebug").start() end
 
+local img
 
 --This function is called only once when the application is started.
 function love.load()
-    
-  end
+  img = love.graphics.newImage("images/heros.png")
+end
   
-  --this function is called once per frame
-  function love.update(dt)
+--this function is called once per frame
+function love.update(dt)
     
-  end
+end
   
-  --this function is used for display guidelines and she is called once per frame
-  function love.draw()
-    
-  end
+--this function is used for display guidelines and she is called once per frame
+function love.draw()
+    --for display the hero with the function img
+    love.graphics.draw(img,100,100)
+end

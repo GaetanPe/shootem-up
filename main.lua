6ce3bcd9ae643ceb41ac505e6d4194a99f3b1c33
GaetanPe <gaetan.peronnet@gmail.com>
2022-05-17 16:52:33 +0200

    basic code skeleton

--- main.lua
+++ main.lua
@@ -5,3 +5,18 @@ love.graphics.setDefaultFilter("nearest")
 
 if arg[#arg] == "-debug" then require("modebug").start() end
 
+
+--This function is called only once when the application is started.
+function love.load()
+    
+  end
+  
+  --this function is called once per frame
+  function love.update(dt)
+    
+  end
+  
+  --this function is used for display guidelines and she is called once per frame
+  function love.draw()
+    
+  end
